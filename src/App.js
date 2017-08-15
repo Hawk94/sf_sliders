80f0358c581c52c0e2f7906eb306dd1cceeb5bd4
Hawk94 <x99tom.miller@gmail.com>
2017-08-15 15:13:58 +0100

    Messing around with styling;

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import React, { Component } from 'react';
-import logo from './sparesFinder.png';
+import logo from './sparesfinder_logo.png';
+import twitterLogo from './images/icon-twitter.svg'
+import telegramLogo from './images/icon-telegram.svg'
+import facebookLogo from './images/icon-facebook.svg'
+import headerCurve from './images/header-curve.svg'
 
-import './App.css';
+import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </div>
-        <p className="App-intro">
+      <div className='App'>
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+          </div>
+          <header>
+            <nav>
+              <a href='#' class='join signup'>Join alpha</a>
+              <a href='https://twitter.com/dustcrypto' class='social'><img src={twitterLogo} alt='Twitter' /></a>
+              <a href='https://www.facebook.com/Dust-108192633212782/' class='social'><img src={facebookLogo} alt='Facebook' /></a>
+              <a href='https://t.me/dustapp' class='social'><img src={telegramLogo} alt='Telegram' /></a>
+            </nav>
+            <div class='curve'><img src={headerCurve} alt='' /></div>
+          </header>
+        <p className='App-intro'>
           Information is power
         </p>
       </div>
-    );
+    )
   }
 }
 
