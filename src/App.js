e4048264956639d2835e9467119d5b154ce19b43
Hawk94 <x99tom.miller@gmail.com>
2017-08-16 09:25:14 +0100

    Still fiddling with layout

--- src/App.js
+++ src/App.js
@@ -11,21 +11,14 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-          <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-          </div>
-          <header>
+          <div className='App-header'>
             <nav>
-              <a href='#' class='join signup'>Join alpha</a>
-              <a href='https://twitter.com/dustcrypto' class='social'><img src={twitterLogo} alt='Twitter' /></a>
-              <a href='https://www.facebook.com/Dust-108192633212782/' class='social'><img src={facebookLogo} alt='Facebook' /></a>
-              <a href='https://t.me/dustapp' class='social'><img src={telegramLogo} alt='Telegram' /></a>
+              <img src={logo} className='App-logo' alt='logo' />
             </nav>
-            <div class='curve'><img src={headerCurve} alt='' /></div>
-          </header>
-        <p className='App-intro'>
-          Information is power
-        </p>
+            <img src={headerCurve} className='curve' alt='' />
+          </div>
+        <div className='App-intro'>
+        </div>
       </div>
     )
   }
